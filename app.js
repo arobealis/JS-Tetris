699ec33ac3c2415f05cd67cf40f1366ee3104172
Nathan <aborealis.688@gmail.com>
2023-01-24 13:40:17 -0800

    set up some basic variables to hold the grid, score, and a few other details

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const scoreDisplay = document.querySelector('#score');
+    const startBtn = document.querySelector('#start-button');
+    const width = 10;
+    let squares = Array.from(document.querySelectorAll('.grid div'));
 
 
     
