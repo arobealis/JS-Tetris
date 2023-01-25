0ab6e594a65d9e2f74297b65e0911e4b5c2ec144
Nathan <aborealis.688@gmail.com>
2023-01-24 17:01:41 -0800

    added function for moving tetromino right

--- app.js
+++ app.js
@@ -64,6 +64,20 @@ document.addEventListener('DOMContentLoaded', () => {
     //make the tetromino move down every seocnd
     timerId = setInterval(moveDown, 1000);
 
+    //assign functions to keyCodes
+    function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft();
+        } else if (e.keyCode === 38) {
+            //rotate();
+        } else if (e.keyCode === 39) {
+            moveRight();
+        } else if (e.keyCode === 40) {
+            moveDown();
+        }
+    }
+    document.addEventListener('keyup', control);
+
     // move down function
     function moveDown() {
         undraw();
@@ -98,7 +112,21 @@ document.addEventListener('DOMContentLoaded', () => {
         draw();
     }
 
+    //move tetromino right, unless it's at the edge or another piece is blocking
+    function moveRight() {
+        undraw();
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1);
 
+        if (!isAtRightEdge) currentPosition += 1;
+
+        if (current.some(index =>  squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -= 1;
+        }
+
+        draw();
+    }
+
+    //
 
 
 
